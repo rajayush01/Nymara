887fe1f7a5f1f83c66e03a831e740a6e42695b52
Ayush Raj <119657235+rajayush01@users.noreply.github.com>
2025-10-11 04:45:42 +0530

    Delete src directory

--- src/types/modules.d.ts
+++
@@ -1,7 +0,0 @@
-// This file helps TypeScript understand our module paths
-declare module '@/pages/Home';
-declare module '@/pages/AboutUs';
-declare module '@/pages/ContactUs';
-declare module '@/pages/NotFound';
-
-// Add any other module declarations here as needed
